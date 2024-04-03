bd9d08364caa7348c37ce3742c076d33238ade35
k.piliuk <k.piliuk@eywa.fi>
2024-04-03 20:23:34 +0300

    permit test

--- scripts/helper.ts
+++ scripts/helper.ts
@@ -1,5 +1,12 @@
 import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
 import { ethers } from "hardhat";
+import { XyroToken } from "../typechain-types/contracts/XyroToken";
+
+interface VRS {
+  r: string;
+  s: string;
+  v: number;
+}
 
 export async function wrapFnc(params: any, fnc: any) {
   let deployer: HardhatEthersSigner;
@@ -42,7 +49,7 @@ export async function wrapFnc(params: any, fnc: any) {
         if (params.length > 1) {
           res = await fnc.deploy(...params);
         } else {
-          res = await fnc.deploy(0, ...params);
+          res = await fnc.deploy(...params);
         }
       } else {
         let isFulfilled = false;
@@ -124,6 +131,77 @@ export async function wrapFnc(params: any, fnc: any) {
   }
 
   throw new Error("Interrupted: Something went wrong!");
+}
+
+export function splitSignatureToVRS(signature: string): VRS {
+  const r = "0x" + signature.substring(2).substring(0, 64);
+  const s = "0x" + signature.substring(2).substring(64, 128);
+  const v = parseInt(signature.substring(2).substring(128, 130), 16);
+
+  return { r, s, v };
+}
+
+export async function getPermitSignature(
+  wallet: HardhatEthersSigner,
+  token: XyroToken,
+  spender: string,
+  value = ethers.MaxUint256,
+  deadline = ethers.MaxUint256,
+  permitConfig?: {
+    nonce?: number;
+    name?: string;
+    chainId?: number;
+    version?: string;
+  }
+): Promise<VRS> {
+  const [nonce, name, version, chainId] = await Promise.all([
+    permitConfig?.nonce ?? token.nonces(wallet.address),
+    permitConfig?.name ?? token.name(),
+    permitConfig?.version ?? "1",
+    permitConfig?.chainId ?? "1337",
+  ]);
+
+  return splitSignatureToVRS(
+    await wallet.signTypedData(
+      {
+        name,
+        version,
+        chainId,
+        verifyingContract: await token.getAddress(),
+      },
+      {
+        Permit: [
+          {
+            name: "owner",
+            type: "address",
+          },
+          {
+            name: "spender",
+            type: "address",
+          },
+          {
+            name: "value",
+            type: "uint256",
+          },
+          {
+            name: "nonce",
+            type: "uint256",
+          },
+          {
+            name: "deadline",
+            type: "uint256",
+          },
+        ],
+      },
+      {
+        owner: wallet.address,
+        spender,
+        value,
+        nonce,
+        deadline,
+      }
+    )
+  );
 }
 
 function timeout(ms: number) {
---
+++ test/testPermit.ts
@@ -0,0 +1,49 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
+import { XyroToken } from "../typechain-types/contracts/XyroToken";
+import { XyroToken__factory } from "../typechain-types/factories/contracts/XyroToken__factory";
+import { getPermitSignature } from "../scripts/helper";
+const parse18 = ethers.parseEther;
+
+describe("Permit", () => {
+  let alice: HardhatEthersSigner;
+  let owner: HardhatEthersSigner;
+  let XyroToken: XyroToken;
+  before(async () => {
+    [owner, alice] = await ethers.getSigners();
+    XyroToken = await new XyroToken__factory(owner).deploy(
+      parse18((1e9).toString())
+    );
+  });
+
+  it("Should transfer with permit", async function () {
+    const spender = alice.address;
+    const amount = parse18("10");
+    const deadline = Math.floor(Date.now() / 1000) + 1000;
+    const result = await getPermitSignature(
+      owner,
+      XyroToken,
+      spender,
+      amount,
+      BigInt(deadline)
+    );
+    await XyroToken.permit(
+      owner.address,
+      spender,
+      amount,
+      deadline,
+      result.v,
+      result.r,
+      result.s
+    );
+    const oldBalance = await XyroToken.balanceOf(alice.address);
+    await XyroToken.connect(alice).transferFrom(
+      owner.address,
+      alice.address,
+      amount
+    );
+    const newBalance = await XyroToken.balanceOf(alice.address);
+    expect(newBalance).to.be.above(oldBalance);
+  });
+});

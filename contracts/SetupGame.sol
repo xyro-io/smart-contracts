2da296569133709efe29b848e93c9c4f37aba159
k.piliuk <k.piliuk@eywa.fi>
2024-05-06 16:40:21 +0300

    natspec fix

--- contracts/SetupGame.sol
+++ contracts/SetupGame.sol
@@ -61,16 +61,16 @@ contract SetupGame is Ownable {
     address public treasury;
 
     /**
-    * @param isStopLoss if stop loss = true, take profit = false
-    * @param startTime when the game will start
-    * @param endTime when the game will end
-    * @param takeProfitPrice take profit price
-    * @param stopLossPrice stop loss price
-    * @param initiator game creator
-    * @param initiator's bet amount
-    * @param unverifiedReport Chainlink DataStreams report
-    * @param newTreasury new treasury address
-    */
+     * @param isStopLoss if stop loss = true, take profit = false
+     * @param startTime when the game will start
+     * @param endTime when the game will end
+     * @param takeProfitPrice take profit price
+     * @param stopLossPrice stop loss price
+     * @param initiator game creator
+     * @param initiator's bet amount
+     * @param unverifiedReport Chainlink DataStreams report
+     * @param newTreasury new treasury address
+     */
     constructor(
         bool isStopLoss,
         uint48 startTime,
@@ -105,10 +105,10 @@ contract SetupGame is Ownable {
     }
 
     /**
-    * Take participation in setup game
-    * @param isStopLoss if stop loss = true, take profit = false
-    * @param sender's bet amount
-    */
+     * Take participation in setup game
+     * @param isStopLoss if stop loss = true, take profit = false
+     * @param amount sender's bet amount
+     */
     function bet(bool isStopLoss, uint256 amount) public {
         require(game.gameStatus == Status.Created, "Wrong status!");
         require(
@@ -130,8 +130,8 @@ contract SetupGame is Ownable {
     }
 
     /**
-    * Closes setup game
-    */
+     * Closes setup game
+     */
     function closeGame() public {
         require(game.initiator == msg.sender, "Wrong sender");
         require(
@@ -152,9 +152,9 @@ contract SetupGame is Ownable {
     }
 
     /**
-    * Finalizes setup game
-    * @param unverifiedReport Chainlink DataStreams report
-    */
+     * Finalizes setup game
+     * @param unverifiedReport Chainlink DataStreams report
+     */
     function finalizeGame(bytes memory unverifiedReport) public onlyOwner {
         require(game.gameStatus == Status.Created, "Wrong status!");
         address upkeep = ITreasury(treasury).upkeep();
@@ -168,7 +168,7 @@ contract SetupGame is Ownable {
         bool takeProfitWon;
         if (game.isStopLoss) {
             if (finalPrice <= game.stopLossPrice) {
-                * sl team wins
+                // sl team wins
                 uint256 finalRate = ITreasury(treasury).calculateSetupRate(
                     game.totalBetsTP,
                     game.totalBetsSL,
@@ -199,7 +199,7 @@ contract SetupGame is Ownable {
             }
         } else {
             if (finalPrice >= game.takeProfitPrice) {
-                * tp team wins
+                // tp team wins
                 uint256 finalRate = ITreasury(treasury).calculateSetupRate(
                     game.totalBetsSL,
                     game.totalBetsTP,
@@ -214,7 +214,7 @@ contract SetupGame is Ownable {
                 }
                 takeProfitWon = true;
             } else {
-                * sl team wins
+                // sl team wins
                 uint256 finalRate = ITreasury(treasury).calculateSetupRate(
                     game.totalBetsTP,
                     game.totalBetsSL,
@@ -247,9 +247,9 @@ contract SetupGame is Ownable {
     }
 
     /**
-    * Change treasury address
-    * @param newTreasury new treasury address
-    */
+     * Change treasury address
+     * @param newTreasury new treasury address
+     */
     function setTreasury(address newTreasury) public onlyOwner {
         treasury = newTreasury;
     }

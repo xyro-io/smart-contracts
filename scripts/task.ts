4b753993de2310f63d258efa18c704b8944ec563
k.piliuk <k.piliuk@eywa.fi>
2024-05-07 16:06:48 +0300

    updated task for feedId

--- scripts/task.ts
+++ scripts/task.ts
@@ -15,7 +15,7 @@ task("balance", "Prints an account's balance")
 task("approveTreasury", "Increases allowance")
   .addParam("account", "Token owner address")
   .setAction(async (taskArgs) => {
-    const signer = await ethers.getSigner(taskArgs.owner);
+    const signer = await ethers.getSigner(taskArgs.account);
     const USDC = await ethers.getContractAt(
       "MockToken",
       contracts.USDC.address
@@ -40,6 +40,7 @@ task("mint", "Mints tokens on target address")
 task("startBullseye", "Starts bullseye game")
   .addParam("time", "How long game will be opened")
   .addParam("betamount", "Bet amount")
+  .addParam("feedId", "Pair feed id")
   .setAction(async (taskArgs: any) => {
     const contract = await ethers.getContractAt(
       "BullseyeGame",
@@ -51,7 +52,8 @@ task("startBullseye", "Starts bullseye game")
       ).timestamp,
       (await ethers.provider.getBlock("latest")).timestamp +
         Number(taskArgs.time),
-      ethers.parseEther(taskArgs.betamount)
+      ethers.parseEther(taskArgs.betamount),
+      taskArgs.feedId
     );
   });
 
@@ -67,14 +69,16 @@ task("betBullseye", "Bullseye bet")
     await contract.connect(signer).bet(taskArgs.price);
   });
 
-task("finalizeBullseye", "Finishes bullseye game").setAction(
+task("finalizeBullseye", "Finishes bullseye game")
+.addParam("feedId", "Price feed id")
+.setAction(
   async (taskArgs: any) => {
     const contract = await ethers.getContractAt(
       "BullseyeGame",
       contracts.BullseyeGame.address
     );
     const price = await getPrice();
-    await contract.finalizeGame(abiEncodeInt192(price));
+    await contract.finalizeGame(abiEncodeInt192(price, taskArgs.feedId));
   }
 );
 
@@ -92,7 +96,7 @@ task("startUpDown", "Starts updown game")
       "UpDownGame",
       contracts.UpDown.address
     );
-    const price = await getPrice();
+    // const price = await getPrice();
     await contract.startGame(
       (
         await ethers.provider.getBlock("latest")
@@ -100,7 +104,9 @@ task("startUpDown", "Starts updown game")
       (await ethers.provider.getBlock("latest")).timestamp +
         Number(taskArgs.time),
       ethers.parseEther(taskArgs.betamount),
-      abiEncodeInt192(price)
+      "0x00062e9d9e815f24d8d23cf51c8d7fced51262153cae9e5eea6c7d503688a101000000000000000000000000000000000000000000000000000000002366910a000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000e0000000000000000000000000000000000000000000000000000000000000022000000000000000000000000000000000000000000000000000000000000002800101000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000012000037da06d56d083fe599397a4769a042d63aa73dc4ef57709d31e9971a5b43900000000000000000000000000000000000000000000000000000000663a0a6200000000000000000000000000000000000000000000000000000000663a0a6200000000000000000000000000000000000000000000000000001d5852c0628800000000000000000000000000000000000000000000000000188dfe5e4f37e400000000000000000000000000000000000000000000000000000000663b5be2000000000000000000000000000000000000000000000d937f6339d313321380000000000000000000000000000000000000000000000d937dc1b888969dc800000000000000000000000000000000000000000000000d938104bb1d8fa7da800000000000000000000000000000000000000000000000000000000000000002e056159d54d44764546e937e161f42a63a3f03a0ee135f787398a2693e2b0680b48840ffb9a18e167115acd4412416b5c89fe3664476598e84d3416a9f02f4f100000000000000000000000000000000000000000000000000000000000000027de3072ac120bebd8a82eee5fda10d512d720a65bba4f6716958eacefb24136b607686e9c98b8be8a7e0f0c2f552bed3a6f83c4d313095f76a65278161a53430",
+      "0x00037da06d56d083fe599397a4769a042d63aa73dc4ef57709d31e9971a5b439",
+      {gasLimit:300_000}
     );
   });
 
@@ -116,14 +122,16 @@ task("betUpDown", "UpDown bet")
     await contract.connect(signer).bet(taskArgs.up === "true");
   });
 
-task("finalizeUpDown", "Finishes UpDown game").setAction(
+task("finalizeUpDown", "Finishes UpDown game")
+.addParam("feedId", "Price feed id")
+.setAction(
   async (taskArgs: any) => {
     const contract = await ethers.getContractAt(
       "UpDownGame",
       contracts.UpDown.address
     );
     const price = await getPrice();
-    await contract.finalizeGame(abiEncodeInt192(price));
+    await contract.finalizeGame(abiEncodeInt192(price, taskArgs.feedId));
   }
 );
 
@@ -164,13 +172,14 @@ task("betExact", "Accept exact price bet")
 
 task("finalizeExact", "Finalize exact price game")
   .addParam("id", "Bet id")
+  .addParam("feedId", "Price feed id")
   .setAction(async (taskArgs: any) => {
     const contract = await ethers.getContractAt(
       "ExactPriceStandalone",
       contracts.ExactPriceOneVsOne.address
     );
     const price = await getPrice();
-    await contract.finalizeGame(taskArgs.id, abiEncodeInt192(price));
+    await contract.finalizeGame(taskArgs.id, abiEncodeInt192(price, taskArgs.feedId));
   });
 
 task("startUpDown1vs1", "Starts one vs one up down game")
@@ -178,6 +187,7 @@ task("startUpDown1vs1", "Starts one vs one up down game")
   .addParam("time", "How long game will be opened")
   .addParam("up", "Will go up?")
   .addParam("betamount", "Bet amount")
+  .addParam("feedId", "Price feed id")
   .setAction(async (taskArgs: any) => {
     const contract = await ethers.getContractAt(
       "UpDownStandalone",
@@ -193,7 +203,7 @@ task("startUpDown1vs1", "Starts one vs one up down game")
         Number(taskArgs.time),
       taskArgs.up === "true",
       ethers.parseEther(taskArgs.betamount),
-      abiEncodeInt192(price)
+      abiEncodeInt192(price, taskArgs.feedId)
     );
   });
 
@@ -211,13 +221,14 @@ task("acceptUpDown", "Accept up down one vs one bet")
 
 task("finalizeUpDown1vs1", "Finalize up down one vs one game")
   .addParam("id", "Bet id")
+  .addParam("feedId", "Price feed id")
   .setAction(async (taskArgs: any) => {
     const contract = await ethers.getContractAt(
       "UpDownStandalone",
       contracts.UpDownOneVsOne.address
     );
     const price = await getPrice();
-    await contract.finalizeGame(taskArgs.id, abiEncodeInt192(price));
+    await contract.finalizeGame(taskArgs.id, abiEncodeInt192(price, taskArgs.feedId));
   });
 
 task("createSetup", "Create setup game")
@@ -226,6 +237,7 @@ task("createSetup", "Create setup game")
   .addParam("slprice", "SL price")
   .addParam("tpprice", "TP price")
   .addParam("betamount", "Bet amount")
+  .addParam("feedId", "Price feed id")
   .setAction(async (taskArgs: any) => {
     const contract = await ethers.getContractAt(
       "GameFactory",
@@ -244,7 +256,7 @@ task("createSetup", "Create setup game")
       ethers.parseEther(taskArgs.slprice),
       ethers.parseEther(taskArgs.betamount),
       taskArgs.sl === "true",
-      abiEncodeInt192(price)
+      abiEncodeInt192(price, taskArgs.feedId)
     );
 
     const treasury = await ethers.getContractAt(
@@ -288,13 +300,14 @@ task("betSetup", "Setup bet")
 task("finalizeSetup", "Finalize up down one vs one game")
   .addParam("address", "Setup address")
   .addParam("price", "Final price")
+  .addParam("feedId", "Price feed id")
   .setAction(async (taskArgs: any) => {
     const contract = await ethers.getContractAt("SetupGame", taskArgs.address);
     const price = await getPrice();
-    await contract.finalizeGame(abiEncodeInt192(price));
+    await contract.finalizeGame(abiEncodeInt192(price, taskArgs.feedId));
   });
 
-function abiEncodeInt192(num: string): string {
-  const encoded = ethers.solidityPacked(["int192"], [num]);
+function abiEncodeInt192(price: string, feedId: string): string {
+  const encoded = ethers.solidityPacked(["int192", "bytes32"], [price, feedId]);
   return encoded.slice(0, 3) + "0".repeat(16) + encoded.slice(3);
 }

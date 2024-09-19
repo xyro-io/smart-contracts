2b9e2905e5bfa760091c9f62dea3d989f3f818ed
k.piliuk <k.piliuk@eywa.fi>
2024-09-19 16:28:08 +0300

    ChainlinkUpkeep finalized

--- contracts/DataStreamsVerifier.sol
+++ contracts/DataStreamsVerifier.sol
@@ -1,20 +1,15 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.19;
+pragma solidity ^0.8.19;
 
 import {Common} from "@chainlink/contracts/src/v0.8/llo-feeds/libraries/Common.sol";
 import {IRewardManager} from "@chainlink/contracts/src/v0.8/llo-feeds/interfaces/IRewardManager.sol";
 import {IVerifierFeeManager} from "@chainlink/contracts/src/v0.8/llo-feeds/interfaces/IVerifierFeeManager.sol";
 import {IERC20} from "@chainlink/contracts-ccip/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@chainlink/contracts-ccip/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/utils/SafeERC20.sol";
-// import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+import {AccessControl} from "./utils/AccessControl.sol";
 
 using SafeERC20 for IERC20;
-
-/**
- * THIS IS AN EXAMPLE CONTRACT THAT USES UN-AUDITED CODE FOR DEMONSTRATION PURPOSES.
- * DO NOT USE THIS CODE IN PRODUCTION.
- */
 
 // Custom interfaces for IVerifierProxy and IFeeManager
 interface IVerifierProxy {
@@ -78,30 +73,25 @@ interface IFeeManager {
  * @dev This contract implements functionality to verify Data Streams reports from
  * the Streams Direct API or WebSocket connection, with payment in LINK tokens.
  */
-contract DataStreamsVerifier {
+contract DataStreamsVerifier is AccessControl {
     error NothingToWithdraw(); // Thrown when a withdrawal attempt is made but the contract holds no tokens of the specified type.
     error NotOwner(address caller); // Thrown when a caller tries to execute a function that is restricted to the contract's owner.
 
-    struct BasicReport {
+    /**
+     * @dev Represents a data report from a Data Streams feed.
+     * The `price`, `bid`, and `ask` values are carried to either 8 or 18 decimal places, depending on the feed.
+     * For more information, see https://docs.chain.link/data-streams/stream-ids.
+     */
+    struct Report {
         bytes32 feedId; // The feed ID the report has data for
         uint32 validFromTimestamp; // Earliest timestamp for which price is applicable
         uint32 observationsTimestamp; // Latest timestamp for which price is applicable
         uint192 nativeFee; // Base cost to validate a transaction using the report, denominated in the chain’s native token (WETH/ETH)
         uint192 linkFee; // Base cost to validate a transaction using the report, denominated in LINK
         uint32 expiresAt; // Latest timestamp where the report can be verified onchain
-        int192 price; // DON consensus median price, carried to 8 decimal places
-    }
-
-    struct PremiumReport {
-        bytes32 feedId; // The feed ID the report has data for
-        uint32 validFromTimestamp; // Earliest timestamp for which price is applicable
-        uint32 observationsTimestamp; // Latest timestamp for which price is applicable
-        uint192 nativeFee; // Base cost to validate a transaction using the report, denominated in the chain’s native token (WETH/ETH)
-        uint192 linkFee; // Base cost to validate a transaction using the report, denominated in LINK
-        uint32 expiresAt; // Latest timestamp where the report can be verified onchain
-        int192 price; // DON consensus median price, carried to 8 decimal places
-        int192 bid; // Simulated price impact of a buy order up to the X% depth of liquidity utilisation
-        int192 ask; // Simulated price impact of a sell order up to the X% depth of liquidity utilisation
+        int192 price; // DON consensus median price (8 or 18 decimals)
+        int192 bid; // Simulated price impact of a buy order up to the X% depth of liquidity utilisation (8 or 18 decimals)
+        int192 ask; // Simulated price impact of a sell order up to the X% depth of liquidity utilisation (8 or 18 decimals)
     }
 
     mapping(uint8 => bytes32) public assetId;
@@ -110,7 +100,6 @@ contract DataStreamsVerifier {
     IVerifierProxy public s_verifier;
     int192 public last_decoded_price;
     uint32 public last_validFromTimestamp;
-    // bytes32 public constant DISTRIBUTOR_ROLE = keccak256("DISTRIBUTOR_ROLE");
 
     event DecodedData(int192, bytes32);
 
@@ -118,20 +107,15 @@ contract DataStreamsVerifier {
      * You can find these addresses on https://docs.chain.link/data-streams/stream-ids
      */
     constructor(address verifier) {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         s_owner = msg.sender;
         s_verifier = IVerifierProxy(verifier);
-    }
-
-    /// @notice Checks if the caller is the owner of the contract.
-    modifier onlyOwner() {
-        if (msg.sender != s_owner) revert NotOwner(msg.sender);
-        _;
     }
 
     function verifyReportWithTimestamp(
         bytes memory unverifiedReport,
         uint8 feedNumber
-    ) external returns (int192, uint32) {
+    ) external onlyRole(DEFAULT_ADMIN_ROLE) returns (int192, uint32) {
         // Report verification fees
         IFeeManager feeManager = IFeeManager(
             address(s_verifier.s_feeManager())
@@ -159,12 +143,9 @@ contract DataStreamsVerifier {
             unverifiedReport,
             abi.encode(feeTokenAddress)
         );
-        // Decode verified report data into BasicReport struct
+        // Decode verified report data into Report struct
         // If your report is a PremiumReport, you should decode it as a PremiumReport
-        BasicReport memory verifiedReport = abi.decode(
-            verifiedReportData,
-            (BasicReport)
-        );
+        Report memory verifiedReport = abi.decode(verifiedReportData, (Report));
         require(
             verifiedReport.feedId == assetId[feedNumber],
             "Wrong feed number"
@@ -181,7 +162,7 @@ contract DataStreamsVerifier {
     function verifyReport(
         bytes memory unverifiedReport,
         uint8 feedNumber
-    ) external returns (int192) {
+    ) external onlyRole(DEFAULT_ADMIN_ROLE) returns (int192) {
         // Report verification fees
         IFeeManager feeManager = IFeeManager(
             address(s_verifier.s_feeManager())
@@ -191,8 +172,10 @@ contract DataStreamsVerifier {
             address(feeManager.i_rewardManager())
         );
 
-        (, /* bytes32[3] reportContextData */ bytes memory reportData) = abi
-            .decode(unverifiedReport, (bytes32[3], bytes));
+        (, bytes memory reportData) = abi.decode(
+            unverifiedReport,
+            (bytes32[3], bytes)
+        );
 
         address feeTokenAddress = feeManager.i_linkAddress();
 
@@ -209,12 +192,8 @@ contract DataStreamsVerifier {
             unverifiedReport,
             abi.encode(feeTokenAddress)
         );
-        // Decode verified report data into BasicReport struct
-        // If your report is a PremiumReport, you should decode it as a PremiumReport
-        BasicReport memory verifiedReport = abi.decode(
-            verifiedReportData,
-            (BasicReport)
-        );
+        // Decode verified report data into Report struct
+        Report memory verifiedReport = abi.decode(verifiedReportData, (Report));
 
         require(
             verifiedReport.feedId == assetId[feedNumber],
@@ -231,11 +210,13 @@ contract DataStreamsVerifier {
     function setfeedNumber(
         uint8 feedNumber,
         bytes32 _assetId
-    ) public onlyOwner {
+    ) public onlyRole(DEFAULT_ADMIN_ROLE) {
         assetId[feedNumber] = _assetId;
     }
 
-    function setfeedNumberBatch(bytes32[] memory _assetIds) public onlyOwner {
+    function setfeedNumberBatch(
+        bytes32[] memory _assetIds
+    ) public onlyRole(DEFAULT_ADMIN_ROLE) {
         for (uint8 i; i < _assetIds.length; i++) {
             assetId[i] = _assetIds[i];
         }
@@ -250,7 +231,7 @@ contract DataStreamsVerifier {
     function withdrawToken(
         address _beneficiary,
         address _token // LINK token address on Arbitrum Sepolia: 0x779877A7B0D9E8603169DdbD7836e478b4624789
-    ) public onlyOwner {
+    ) public onlyRole(DEFAULT_ADMIN_ROLE) {
         // Retrieve the balance of this contract
         uint256 amount = IERC20(_token).balanceOf(address(this));
 

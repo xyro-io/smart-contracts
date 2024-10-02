4141ef214d3bb40498a64b4bbd588d67f8c0b6b3
k.piliuk <k.piliuk@eywa.fi>
2024-10-02 03:02:39 +0300

    XyroToke: mainnet deploy script

---
+++ scripts/tokenDeploy.ts
@@ -0,0 +1,11 @@
+import { wrapFnc } from "./helper";
+import { ethers } from "hardhat";
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+  console.log("Deployer = ", deployer.address);
+  const factory = await ethers.getContractFactory("XyroToken");
+  let XyroToken = await wrapFnc([parse18((1e9).toString())], factory);
+  console.log("XyroToken deployed = ", XyroToken.target);
+}
+main();

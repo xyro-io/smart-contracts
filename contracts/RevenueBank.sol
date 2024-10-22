026520540fdb3f102d922cccc822b532f66665eb
k.piliuk <k.piliuk@eywa.fi>
2024-10-22 16:15:18 +0300

    draft: unfinished uniswap integration

--- contracts/RevenueBank.sol
+++ contracts/RevenueBank.sol
@@ -19,6 +19,13 @@ interface ITreasury {
 contract RevenueBank is AccessControl, EIP712, Nonces {
     using ECDSA for bytes32;
     event NewSigner(address newSigner);
+    bytes32 public constant ACCOUNTANT_ROLE = keccak256("ACCOUNTANT_ROLE");
+    uint256 public constant FEE_DENOMINATOR = 10000;
+    uint256 public buybackBalance;
+    uint256 public rewardsBalance;
+    uint256 public collectedFees;
+    uint256 public buybackPart = 2500;
+    uint256 public rewardsPart = 500;
     address public approvedToken;
     address public xyroToken;
     address public signer;
@@ -43,7 +50,9 @@ contract RevenueBank is AccessControl, EIP712, Nonces {
     function withdraw(
         uint256 amount,
         address to
-    ) public onlyRole(DEFAULT_ADMIN_ROLE) {
+    ) public onlyRole(ACCOUNTANT_ROLE) {
+        require(collectedFees >= amount, "Wrong amount");
+        collectedFees -= amount;
         SafeERC20.safeTransfer(IERC20(approvedToken), to, amount);
     }
 
@@ -74,13 +83,26 @@ contract RevenueBank is AccessControl, EIP712, Nonces {
         SafeERC20.safeTransfer(IERC20(xyroToken), data.to, data.amount);
     }
 
-    function collectFees(uint256 amount) public onlyRole(DEFAULT_ADMIN_ROLE) {
+    function collectFees(uint256 amount) public onlyRole(ACCOUNTANT_ROLE) {
         ITreasury(treasury).withdrawFees(msg.sender, amount);
+        uint256 amountForBuyback = (amount * buybackPart) / FEE_DENOMINATOR;
+        buybackBalance += amountForBuyback;
+        uint256 amountForRewards = (amount * rewardsPart) / FEE_DENOMINATOR;
+        rewardsBalance += amountForRewards;
+        collectedFees += amount - amountForRewards - amountForBuyback;
     }
 
     function setSigner(address newSigner) public onlyRole(DEFAULT_ADMIN_ROLE) {
         signer = newSigner;
         emit NewSigner(newSigner);
+    }
+
+    function setFeeDistribution(
+        uint256 newBuyBackPart,
+        uint256 newRewardPart
+    ) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        buybackPart = newBuyBackPart;
+        rewardsPart = newRewardPart;
     }
 
     function nonces(

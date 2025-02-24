3e7d71b5e672e2192407139031232defea545342
diamondhare <62943037+diamondhare@users.noreply.github.com>
2025-02-25 01:27:14 +0300

    Require Bullseye fix

--- contracts/Bullseye.sol
+++ contracts/Bullseye.sol
@@ -113,7 +113,7 @@ contract Bullseye is AccessControl {
             ) != bytes32(0),
             "Wrong feed number"
         );
-        require(range >= 10 ** pricePrecision, "Invalid exact range");
+        require(range >= 10 ** precision, "Invalid exact range");
         exactRange = range;
         pricePrecision = precision;
         packedData = (block.timestamp |
